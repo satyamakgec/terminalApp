9f538ac3c06ec78dbd7764cf19e249d277d7f63a
Charles St.Louis <12cpsl@queensu.ca>
2019-02-07 23:07:33 -0500

    Changed header tabs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -36,9 +36,9 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'intro/creating-account', label: 'Docs'},
-    {doc: 'apis/apis-creation', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {doc: 'apis/apis-creation', label: 'Tutorials'},
+    {page: 'help', label: 'Support'},
+    {blog: true, label: 'Community'},
   ],
 
   // If you have users set above, you add it here:
